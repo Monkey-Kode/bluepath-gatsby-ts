d171334a370d008f2d781872a754b1bd4b0c922d
Jull Weber <jull@monkeykode.com>
2024-09-10 19:33:26 -0700

    feat(PageContent): Handle TOC and null content types

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import Carousel from './Carousel';
-import Plain from './Plain';
-import Projects from './Projects';
-import Video from './Video';
-import Team from './Team';
-import { ArrElement } from '../types';
+import React from "react";
+import Carousel from "./Carousel";
+import Plain from "./Plain";
+import Projects from "./Projects";
+import Video from "./Video";
+import Team from "./Team";
+import { ArrElement } from "../types";
 
 function PageContent({
   content,
 }: {
-  content: ArrElement<Queries.HomeMainQuery['allSanityHomesections']['nodes']>;
+  content: ArrElement<Queries.HomeMainQuery["allSanityHomesections"]["nodes"]>;
 }) {
   const contentType = content?.contentType?.name;
-  if (contentType === 'Video') {
+  if (contentType === "TOC" || !contentType) return null;
+  if (contentType === "Video") {
     return <Video key={content.id} content={content} />;
-  } else if (contentType === 'Carousel') {
+  } else if (contentType === "Carousel") {
     return <Carousel key={content.id} content={content} />;
-  } else if (contentType === 'Projects') {
+  } else if (contentType === "Projects") {
     return <Projects key={content.id} sanityPage={content} />;
-  } else if (contentType === 'Team') {
+  } else if (contentType === "Team") {
     // @ts-ignore
     return <Team key={content.id} sanityPage={content} />;
   } else {
