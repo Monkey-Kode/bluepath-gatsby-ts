11d57c3e361aab710f3c279b3b2ac391c967250e
Jull Weber <jull@monkeykode.com>
2024-09-30 11:45:12 -0700

    style: Improve scrollbar styles and remove unnecessary comments

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,12 +1,12 @@
-import { graphql, useStaticQuery } from 'gatsby';
-import React, { useState, useEffect, SyntheticEvent } from 'react';
-import styled from 'styled-components';
-import sortObject from '../utils/sortObject';
-import TeamCard from './TeamCard';
-import TeamThumbnail from './TeamThumbnail';
-import BackgroundImage from 'gatsby-background-image';
-import { convertToBgImage } from 'gbimage-bridge';
-import { getImage } from 'gatsby-plugin-image';
+import { graphql, useStaticQuery } from "gatsby";
+import React, { useState, useEffect, SyntheticEvent } from "react";
+import styled from "styled-components";
+import sortObject from "../utils/sortObject";
+import TeamCard from "./TeamCard";
+import TeamThumbnail from "./TeamThumbnail";
+import BackgroundImage from "gatsby-background-image";
+import { convertToBgImage } from "gbimage-bridge";
+import { getImage } from "gatsby-plugin-image";
 
 const StyledTeamSection = styled(BackgroundImage)`
   background: rgb(24, 85, 140);
@@ -47,11 +47,29 @@ const StyledTeamSection = styled(BackgroundImage)`
 `;
 
 const StyledThumbs = styled.div`
-  /* @media only screen and (min-width: 1024px) {
-    grid-template-columns: repeat(7, 1fr);
-  } */
   overflow-x: auto;
   scroll-direction: horizontal;
+  padding-block-end: 0.5rem;
+
+  /* Custom scrollbar styles */
+  &::-webkit-scrollbar {
+    height: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transaparent;
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--blue);
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+
   @media only screen and (min-width: 801px) {
     display: grid;
     grid-auto-flow: column;
@@ -62,8 +80,6 @@ const StyledThumbs = styled.div`
     margin: 0 auto;
     > div {
       max-width: 160px;
-      /* min-width: 160px; */
-      /* width: 160px; */
       margin: 0 auto;
     }
   }
@@ -86,12 +102,10 @@ const StyledThumbs = styled.div`
     height: 100%;
     @media only screen and (max-width: 800px) {
       padding: 10% 5%;
-      /* max-width: 233px; */
       margin: 0 auto;
     }
   }
 `;
-
 const StyledInfos = styled.div`
   position: relative;
   width: 100%;
@@ -104,8 +118,8 @@ const StyledInfos = styled.div`
 function Team({ sanityPage }: { sanityPage: Queries.SanityPage }) {
   const { name, background, backgroundColor, mobilebackground } = sanityPage;
   let sectionBg = background;
-  if (typeof window !== 'undefined') {
-    const mql = window.matchMedia('(max-width: 600px)');
+  if (typeof window !== "undefined") {
+    const mql = window.matchMedia("(max-width: 600px)");
     if (!mql.matches && background) {
       sectionBg = background;
     } else if (mql.matches && mobilebackground) {
@@ -115,8 +129,8 @@ function Team({ sanityPage }: { sanityPage: Queries.SanityPage }) {
     }
   }
 
-  const bgColor = backgroundColor ? backgroundColor.hex : 'transparent';
-  const [currentSlide, setcurrentSlide] = useState('');
+  const bgColor = backgroundColor ? backgroundColor.hex : "transparent";
+  const [currentSlide, setcurrentSlide] = useState("");
   const {
     allSanityTeam: { nodes: team },
   }: Queries.TeamQuery = useStaticQuery(graphql`
@@ -142,30 +156,30 @@ function Team({ sanityPage }: { sanityPage: Queries.SanityPage }) {
     }
   `);
   const members = sortObject(
-    team
-  ) as Queries.TeamQuery['allSanityTeam']['nodes'];
+    team,
+  ) as Queries.TeamQuery["allSanityTeam"]["nodes"];
 
   useEffect(() => {
-    const slider = document.getElementById('team-carousel');
+    const slider = document.getElementById("team-carousel");
     let isDown = false;
     let startX: number;
     let scrollLeft: number;
     if (slider) {
-      slider.addEventListener('mousedown', (e) => {
+      slider.addEventListener("mousedown", (e) => {
         isDown = true;
-        slider.classList.add('active');
+        slider.classList.add("active");
         startX = e.pageX - slider.offsetLeft;
         scrollLeft = slider.scrollLeft;
       });
-      slider.addEventListener('mouseleave', () => {
+      slider.addEventListener("mouseleave", () => {
         isDown = false;
-        slider.classList.remove('active');
+        slider.classList.remove("active");
       });
-      slider.addEventListener('mouseup', () => {
+      slider.addEventListener("mouseup", () => {
         isDown = false;
-        slider.classList.remove('active');
+        slider.classList.remove("active");
       });
-      slider.addEventListener('mousemove', (e) => {
+      slider.addEventListener("mousemove", (e) => {
         if (!isDown) return;
         e.preventDefault();
         const x = e.pageX - slider.offsetLeft;
@@ -185,7 +199,7 @@ function Team({ sanityPage }: { sanityPage: Queries.SanityPage }) {
       Tag="section"
       backgroundColor={bgColor}
       onClick={(e: SyntheticEvent) => {
-        setcurrentSlide('');
+        setcurrentSlide("");
       }}
       {...bgImage}
     >
