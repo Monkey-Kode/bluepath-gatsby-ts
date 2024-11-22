54a975310184c7dd687c133c8e3dcf66c1ee4aa8
Jull Weber <jull@monkeykode.com>
2024-11-22 13:04:07 -0800

    refactor(StyleBackgroundImage): Remove animations, update bg size

--- src/styles/StyleBackgroundImage.ts
+++ src/styles/StyleBackgroundImage.ts
@@ -1,38 +1,12 @@
 import BackgroundImage from "gatsby-background-image";
 import styled, { keyframes, css } from "styled-components";
-
-const panAnimation = keyframes`
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-`;
-
-const mobileAnimation = css`
-  background-size: 120% auto;
-  animation: ${panAnimation} 30s ease-in-out infinite;
-`;
-
 const StyleBackgroundImage = styled(BackgroundImage)`
-  /* height: 100vh; */
   background-position: center center;
   background-repeat: no-repeat;
-  background-size: cover;
   background-attachment: fixed !important;
   display: flex;
   align-items: center;
-
-  @media (max-width: 1024px) {
-    &::before,
-    &::after {
-      ${mobileAnimation}
-    }
-  }
+  background-size: contain;
 `;
 
 export default StyleBackgroundImage;
