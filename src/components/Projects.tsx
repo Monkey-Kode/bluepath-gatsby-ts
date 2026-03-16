e66bf325b3f868205df3cda1d6dbf41d7d6feb77
Jull Weber <jull@monkeykode.com>
2026-03-16 13:58:53 -0700

    fix: add error handling and null guards to Google Maps in Projects
    - Add .catch() to loader.load() so Maps API failures are logged
    - Guard against null ref before creating Map instance
    - Skip projects with missing location data instead of crashing
    - Use optional chaining on marker DOM element in click handler

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -80,125 +80,124 @@ function Projects({
       mapOptions.zoom = 3.5;
     }
 
-    loader.load().then(() => {
-      let activeInfoWindow: any;
-      const allMarkers = [];
-      // let currentMark;
-      // const bounds = new window.google.maps.LatLngBounds();
-      const map =
-        ref.current && new window.google.maps.Map(ref.current, mapOptions);
+    loader
+      .load()
+      .then(() => {
+        if (!ref.current) {
+          console.error("Map container ref is not available");
+          return;
+        }
 
-      projects.map((project) => {
-        const infowindow = new window.google.maps.InfoWindow({
-          content: renderToString(<InfoWindow project={project} />),
-        });
-        const iconSize = 15;
-        // console.log(project?.location);
-        // const { lat, lng } = project?.location;
-        // bounds.extend(new window.google.maps.LatLng(lat, lng));
+        let activeInfoWindow: any;
+        const allMarkers: google.maps.Marker[] = [];
+        const map = new window.google.maps.Map(ref.current, mapOptions);
 
-        const icon = {
-          url: mapMarker,
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new window.google.maps.Size(iconSize, iconSize),
-          // The origin for this image is (0, 0).
-          origin: new window.google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-          anchor: new window.google.maps.Point(0, 0),
-          optimized: false,
-        };
-        const marker = new window.google.maps.Marker({
-          position: project.location as any,
-          map,
-          title: allMarkers.length.toString(),
-          icon,
-        });
-
-        allMarkers.push(marker);
-
-        const myoverlay = new window.google.maps.OverlayView();
-        myoverlay.draw = function (this: google.maps.OverlayView) {
-          const panes = this.getPanes();
-          if (panes) {
-            panes.markerLayer.id = "markerLayer";
+        projects.forEach((project) => {
+          if (!project.location?.lat || !project.location?.lng) {
+            console.warn("Skipping project with missing location:", project.id);
+            return;
           }
-        };
-        myoverlay.setMap(map);
 
-        marker.addListener("click", function (this: google.maps.MarkerOptions) {
-          const thisTitle = Number(this.title);
-          const index = thisTitle + 2;
-          // currentMark = this;
-          document.querySelectorAll(`#markerLayer div img`).forEach((img) => {
-            img.classList.remove("grow");
-            img.classList.remove("shrink");
+          const infowindow = new window.google.maps.InfoWindow({
+            content: renderToString(<InfoWindow project={project} />),
+          });
+          const iconSize = 15;
+
+          const icon = {
+            url: mapMarker,
+            size: new window.google.maps.Size(iconSize, iconSize),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(0, 0),
+          };
+          const marker = new window.google.maps.Marker({
+            position: project.location as google.maps.LatLngLiteral,
+            map,
+            title: allMarkers.length.toString(),
+            icon,
           });
 
-          if (activeInfoWindow) {
-            activeInfoWindow.close();
-          }
-          infowindow.open(map, marker);
-          activeInfoWindow = infowindow;
+          allMarkers.push(marker);
 
-          const markerElement = document.querySelector(
-            `#markerLayer div:nth-child(${index}) img`,
-          ) as HTMLElement;
-          markerElement.classList.add("grow");
-        });
+          const myoverlay = new window.google.maps.OverlayView();
+          myoverlay.draw = function (this: google.maps.OverlayView) {
+            const panes = this.getPanes();
+            if (panes) {
+              panes.markerLayer.id = "markerLayer";
+            }
+          };
+          myoverlay.setMap(map);
 
-        marker.addListener(
-          "mouseover",
-          function (this: google.maps.MarkerOptions) {
-            const thisTitle = Number(this.title);
-            const index = thisTitle + 2;
-            // document.querySelectorAll(`#markerLayer div img`).forEach((img) => {
-            //   img.classList.remove('grow');
-            //   img.classList.remove('shrink');
-            // });
-            // console.log('mouseover', index);
-            // console.log('activeInfoWindow', activeInfoWindow);
-            if (!activeInfoWindow) {
-              const image = document.querySelector(
+          marker.addListener(
+            "click",
+            function (this: google.maps.MarkerOptions) {
+              const thisTitle = Number(this.title);
+              const index = thisTitle + 2;
+              document
+                .querySelectorAll(`#markerLayer div img`)
+                .forEach((img) => {
+                  img.classList.remove("grow");
+                  img.classList.remove("shrink");
+                });
+
+              if (activeInfoWindow) {
+                activeInfoWindow.close();
+              }
+              infowindow.open(map, marker);
+              activeInfoWindow = infowindow;
+
+              const markerElement = document.querySelector(
                 `#markerLayer div:nth-child(${index}) img`,
               );
+              markerElement?.classList.add("grow");
+            },
+          );
 
-              image?.classList.remove("shrink");
-              image?.classList.add("grow");
-            }
-          },
-        );
+          marker.addListener(
+            "mouseover",
+            function (this: google.maps.MarkerOptions) {
+              const thisTitle = Number(this.title);
+              const index = thisTitle + 2;
+              if (!activeInfoWindow) {
+                const image = document.querySelector(
+                  `#markerLayer div:nth-child(${index}) img`,
+                );
 
-        marker.addListener(
-          "mouseout",
-          function (this: google.maps.MarkerOptions) {
-            const thisTitle = Number(this.title);
-            const index = thisTitle + 2;
-            if (!activeInfoWindow) {
-              const image = document.querySelector(
-                `#markerLayer div:nth-child(${index}) img`,
-              );
-              image?.classList.remove("grow");
-              image?.classList.add("shrink");
-              image?.classList.remove("shrink");
-            }
-          },
-        );
+                image?.classList.remove("shrink");
+                image?.classList.add("grow");
+              }
+            },
+          );
 
-        infowindow.addListener("closeclick", function () {
-          // console.log(currentMark.title);
-          // const thisTitle = Number(currentMark.title);
-          // const index = thisTitle + 2;
-          document.querySelectorAll(`#markerLayer div img`).forEach((img) => {
-            img.classList.remove("grow");
-            img.classList.remove("shrink");
+          marker.addListener(
+            "mouseout",
+            function (this: google.maps.MarkerOptions) {
+              const thisTitle = Number(this.title);
+              const index = thisTitle + 2;
+              if (!activeInfoWindow) {
+                const image = document.querySelector(
+                  `#markerLayer div:nth-child(${index}) img`,
+                );
+                image?.classList.remove("grow");
+                image?.classList.add("shrink");
+                image?.classList.remove("shrink");
+              }
+            },
+          );
+
+          infowindow.addListener("closeclick", function () {
+            document
+              .querySelectorAll(`#markerLayer div img`)
+              .forEach((img) => {
+                img.classList.remove("grow");
+                img.classList.remove("shrink");
+              });
+            activeInfoWindow = null;
           });
-          activeInfoWindow = null;
         });
-
-        return project;
+      })
+      .catch((err) => {
+        console.error("Google Maps failed to load:", err);
       });
-      // map.fitBounds(bounds);
-    });
   }, [projects]);
 
   return <MapSection id={id} ref={ref}></MapSection>;
